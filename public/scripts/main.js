e51ed9f369c3dcddb1dc2625bbc91d4a10b7d6a4
Unknown <wepale17@gmail.com>
2019-03-11 17:05:19 +0100

    Chat done
    Continue creating one chat for each game

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,6 +8,9 @@ const myVue = new Vue({
     landscape: false,
     previusShow: "sepSchedule",
     actualShow: "sepSchedule",
+    currentUser: "",
+    currentUID: "",
+    db: firebase.database(),
     show: {
       sepSchedule: true,
       octSchedule: false,
@@ -20,11 +23,11 @@ const myVue = new Vue({
       window.addEventListener("orientationchange", () => this.landscape = !this.landscape);
     },
 
-    doOnLandscape(){
+    doOnLandscape() {
       switch (true) {
         case this.actualShow.includes("Game"):
-        this.show[this.gameData.find(game => game.id === this.actualShow).previus] = true;
-        break;
+          this.show[this.gameData.find(game => game.id === this.actualShow).previus] = true;
+          break;
         case this.actualShow.includes("Schedule"):
           this.show[this.gameData.find(game => game.previus === this.actualShow).id] = true;
           break;
@@ -33,7 +36,7 @@ const myVue = new Vue({
       }
     },
 
-    showOnlyThis(keyName){
+    showOnlyThis(keyName) {
       this.show[keyName] = true;
       Object.keys(this.show).filter(key => key !== keyName).forEach(key => this.show[key] = false);
     },
@@ -57,10 +60,77 @@ const myVue = new Vue({
       let data = await response.json()
       //proceed only when the second promise is resolved
       this.gameData = data;
-      this.gameData.forEach(game => this.show[game.actual] = false);
       this.sepGames = data.filter(game => game.month === "september");
       this.octGames = data.filter(game => game.month === "october");
       this.isLoaded = true;
+    },
+
+    //Firebase Auth
+    initFirebaseAuth() {
+      // Listen to auth state changes.
+      firebase.auth().onAuthStateChanged(user => {
+        // We ignore token refresh events.
+        if (user && this.currentUID === user.uid) {
+          return;
+        }
+
+        if (user) { //Log IN
+          this.currentUID = user.uid;
+          this.currentUser = user;
+          this.writeUserData(user.uid, user.displayName, user.photoURL);
+        } else { //Log Out
+          // Set currentUID to null.
+          this.currentUID = null;
+          // Display the splash page where you can sign-in.
+        }
+      });
+    },
+
+    getUserID(){
+      return firebase.auth().currentUser.uid;
+    },
+
+    getProfilePicUrl() {
+      return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+    },
+
+    getUserName() {
+      return firebase.auth().currentUser.displayName;
+    },
+
+    signIn() {
+      // Sign into Firebase using popup auth & Google as the identity provider.
+      firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    },
+
+    signOut() {
+      // Sign out of Firebase.
+      firebase.auth().signOut();
+    },
+
+    //Firebase Database
+    sendMessage() {
+      let msg = document.getElementById('message').value;
+      document.getElementById('message').value = "";
+      this.db.ref('message').push({
+        message: msg,
+        user: this.getUserName(),
+        userID: this.currentUID
+      });
+    },
+
+    writeUserData(userId, name, imageUrl) {
+      firebase.database().ref('users/' + userId).set({
+        username: name,
+        profile_picture: imageUrl
+      });
+    },
+
+    getMessage() {
+      this.db.ref('message').on('child_added', data => {
+        document.getElementById('chat').innerHTML += `<p> ${data.val().user}: ${data.val().message}</p>`;
+        this.user
+      });
     }
   },
 
@@ -78,6 +148,8 @@ const myVue = new Vue({
 
   created() {
     this.getData();
+    this.initFirebaseAuth();
+    this.getMessage();
   },
   beforeUpdate() {
     console.log("Before: " + this.previusShow);
@@ -86,7 +158,5 @@ const myVue = new Vue({
   mounted() {
     this.checkOrientation();
   },
-
-
 
 })
